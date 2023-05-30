6b271f5cd83b6e8d00851011ca2cbf0afa286536
Arash Afshar <arash.afshar.edu@gmail.com>
2023-05-30 09:37:02 -0600

    comment blueprints

--- extension/extension.js
+++ extension/extension.js
@@ -1,12 +1,154 @@
 const vscode = require("vscode");
 const fetch = require("node-fetch");
 
+let commentId = 1;
+
+class NoteComment {
+  id; //: number;
+  label; //: string | undefined;
+  savedBody; //: string | vscode.MarkdownString; // for the Cancel button
+
+  constructor(
+    body,
+    mode,
+    author,
+    parent,
+    contextValue,
+  ) {
+    this.id = ++commentId;
+    this.savedBody = this.body;
+  }
+}
+
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate() {
+function activate(context) {
+  // Backend enpoint
   const endpoint = "http://localhost:3000/review"
 
+  // -------------------------- Comment handling
+  const commentController = vscode.comments.createCommentController('audit-comment', 'Comments/notes during code audits.');
+  context.subscriptions.push(commentController);
+
+  // A `CommentingRangeProvider` controls where gutter decorations that allow adding comments are shown
+  commentController.commentingRangeProvider = {
+    provideCommentingRanges: (document, token) => {
+      const lineCount = document.lineCount;
+      return [new vscode.Range(0, 0, lineCount - 1, 0)];
+    }
+  };
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.createNote', (reply) => {
+    replyNote(reply);
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.replyNote', (reply) => {
+    replyNote(reply);
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.startDraft', (reply) => {
+    const thread = reply.thread;
+    thread.contextValue = 'draft';
+    const newComment = new NoteComment(reply.text, vscode.CommentMode.Preview, { name: 'vscode' }, thread);
+    newComment.label = 'pending';
+    thread.comments = [...thread.comments, newComment];
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.finishDraft', (reply) => {
+    const thread = reply.thread;
+
+    if (!thread) {
+      return;
+    }
+
+    thread.contextValue = undefined;
+    thread.collapsibleState = vscode.CommentThreadCollapsibleState.Collapsed;
+    if (reply.text) {
+      const newComment = new NoteComment(reply.text, vscode.CommentMode.Preview, { name: 'vscode' }, thread);
+      thread.comments = [...thread.comments, newComment].map(comment => {
+        comment.label = undefined;
+        return comment;
+      });
+    }
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.deleteNoteComment', (comment) => {
+    const thread = comment.parent;
+    if (!thread) {
+      return;
+    }
+
+    thread.comments = thread.comments.filter(cmt => cmt.id !== comment.id);
+
+    if (thread.comments.length === 0) {
+      thread.dispose();
+    }
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.deleteNote', (thread) => {
+    thread.dispose();
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.cancelsaveNote', (comment) => {
+    if (!comment.parent) {
+      return;
+    }
+
+    comment.parent.comments = comment.parent.comments.map(cmt => {
+      if (cmt.id === comment.id) {
+        cmt.body = cmt.savedBody;
+        cmt.mode = vscode.CommentMode.Preview;
+      }
+
+      return cmt;
+    });
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.saveNote', (comment) => {
+    if (!comment.parent) {
+      return;
+    }
+
+    comment.parent.comments = comment.parent.comments.map(cmt => {
+      if (cmt.id === comment.id) {
+        cmt.savedBody = cmt.body;
+        cmt.mode = vscode.CommentMode.Preview;
+      }
+
+      return cmt;
+    });
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.editNote', (comment) => {
+    if (!comment.parent) {
+      return;
+    }
+
+    comment.parent.comments = comment.parent.comments.map(cmt => {
+      if (cmt.id === comment.id) {
+        cmt.mode = vscode.CommentMode.Editing;
+      }
+
+      return cmt;
+    });
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand('auditor.dispose', () => {
+    commentController.dispose();
+  }));
+
+  function replyNote(reply) {
+    const thread = reply.thread;
+    const newComment = new NoteComment(reply.text, vscode.CommentMode.Preview, { name: 'vscode' }, thread, thread.comments.length ? 'canDelete' : undefined);
+    if (thread.contextValue === 'draft') {
+      newComment.label = 'pending';
+    }
+
+    thread.comments = [...thread.comments, newComment];
+  }
+
+  // -------------------------- Line review handling
   const reviewedLineDecorationType =
     vscode.window.createTextEditorDecorationType({
       backgroundColor: { id: "auditor.reviewedBackground" },
